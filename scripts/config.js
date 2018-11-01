83c48768a5e42990dc6aab7dfd9983636986978a
Jan Simson <jan.simson@gmx.de>
2018-11-01 18:05:43 +0100

    Generate PDFs into /pdf folder

--- scripts/config.js
+++ scripts/config.js
@@ -4,7 +4,7 @@ const SRC_DIR = path.join(__dirname, '../src/')
 const SRC_DATA = path.join(SRC_DIR, 'data.json')
 const SRC_HTML = path.join(SRC_DIR, 'index.html')
 
-const DIST_DIR = path.join(__dirname, '../dist/')
+const DIST_DIR = path.join(__dirname, '../pdf/')
 const DIST_PDF = path.join(DIST_DIR, 'cards.pdf')
 const DIST_PDF_COMBINED = path.join(DIST_DIR, 'combined.pdf')
 
