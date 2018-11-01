26846531c82fc4c1ceca13b9933a803d65bca024
Jan Simson <jan.simson@gmx.de>
2018-11-01 13:46:21 +0100

    Added icons from game-icons.net

--- getHtml.js
+++ getHtml.js
@@ -5,6 +5,16 @@ const config = require('./config.js')
 const data = require('./src/data.json')
 
 let rawHtml = fs.readFileSync(config.TARGET_FILE, 'utf8')
+
+handlebars.registerHelper('iconNotice', (icon, options) => {
+  if (!icon) { return }
+
+  let split = icon.split('/')
+  // Show only when icon is in a subfolder and  for icons in "game-icons"
+  if (split.length < 3 || split[0] !== 'game-icons') { return }
+
+  return `Icon by ${split[1]}`
+})
 let html =  handlebars.compile(rawHtml)(data)
 
 module.exports = html
