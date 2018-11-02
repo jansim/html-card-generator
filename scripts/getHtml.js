4481257746855975f5b1bf9886e65b13b9fede6d
Jan Simson <jan.simson@gmx.de>
2018-11-03 00:43:39 +0100

    Added repeat parameter

--- scripts/getHtml.js
+++ scripts/getHtml.js
@@ -6,6 +6,17 @@ const data = require(config.SRC_DATA)
 
 let rawHtml = fs.readFileSync(config.SRC_HTML, 'utf8')
 
+// --- Handlebars ---
+// Repeat content X times (undefined => 1)
+// from https://stackoverflow.com/questions/11924452/iterating-over-basic-for-loop-using-handlebars-js
+handlebars.registerHelper('times', function(n, block) {
+    var accum = '';
+    if (n === undefined) { n = 1 }
+    for(var i = 0; i < n; ++i) {
+      accum += block.fn(this);
+    }
+    return accum;
+})
 handlebars.registerHelper('iconNotice', (icon, options) => {
   if (!icon) { return }
 
