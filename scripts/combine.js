8859d370dba25334fe8503255ee96c9e9eb94629
Jan Simson <jan.simson@gmx.de>
2018-11-03 13:06:27 +0100

    Enable reordering to support rendering fronts and backs

--- scripts/combine.js
+++ scripts/combine.js
@@ -4,6 +4,9 @@ const config = require('./config')
 const SRC_PDF_PATH = config.DIST_PDF
 const OUT_PDF_PATH = config.DIST_PDF_COMBINED
 
+// Reorder the cards to properly be able to print fronts and backs?
+const REORDER_FRONTS_BACKS = true
+
 var pdfWriter = hummus.createWriter(OUT_PDF_PATH, {version: hummus.ePDFVersion14})
 
 // Convet mm to pts (the internal pdf unit)
@@ -26,6 +29,41 @@ let page = createNewPage()
 let formIDs = pdfWriter.createFormXObjectsFromPDF(SRC_PDF_PATH, hummus.ePDFPageBoxMediaBox)
 console.log(`Reading ${formIDs.length} PDF Pages`)
 
+// --- Reorder Pages for fronts & backs ---
+if (REORDER_FRONTS_BACKS) {
+  // How many cards are rendered per page? (this is the measure that guides the reordering)
+  const CARDS_PER_PAGE = 10
+  let fronts = formIDs.filter((x,i) => !(i % 2))
+  let backs = formIDs.filter((x,i) => i % 2)
+
+  let reordered = []
+  // Repeat as long as there are still cards
+  while(fronts.length) {
+    // Insert as many cards as defined in CARDS_PER_PAGE (fill with undefined if necessary)
+    // do this for fronts:
+    for (var i = 0; i < CARDS_PER_PAGE; i++) {
+      if (fronts.length) {
+        reordered.push(fronts.shift())
+      } else {
+        reordered.push(undefined)
+      }
+    }
+    // and do this for backs:
+    for (var i = 0; i < CARDS_PER_PAGE; i++) {
+      if (backs.length) {
+        reordered.push(backs.shift())
+      } else {
+        reordered.push(undefined)
+      }
+    }
+  }
+  console.log(`Reordering finished.`)
+  console.log(`[# of cards] previous: ${formIDs.length} | new: ${reordered.length}`)
+  // save reordered in new array
+  formIDs = reordered
+}
+
+// --- Rendering to PDF ---
 // Determine all kinds of dimensions for the target template
 let docWidth = PAGE_WIDTH
 let docHeight = PAGE_HEIGHT
@@ -40,12 +78,14 @@ let y = marginY
 // .cm() parameters [For reference]
 // (scaleX, trans?, trans?, scaleY, xFromLeft, yFromBot)
 formIDs.forEach((ID, index) => {
-  // Place card on page
-  pdfWriter.startPageContentContext(page)
-    .q() // start
-    .cm(1, 0, 0, 1, x, y)
-    .doXObject(page.getResourcesDictionary().addFormXObjectMapping(ID))
-    .Q() //end
+  if (ID !== undefined) {
+    // Place card on page
+    pdfWriter.startPageContentContext(page)
+      .q() // start
+      .cm(1, 0, 0, 1, x, y)
+      .doXObject(page.getResourcesDictionary().addFormXObjectMapping(ID))
+      .Q() //end
+  }
 
   // Increment X position
   x += cardWidth
